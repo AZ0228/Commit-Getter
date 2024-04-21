b642504359e8dc0b119be4b3e73d4d23597c3164
AZ0228 <jbliu88@gmail.com>
2024-04-21 16:29:40 -0400

    fixed fetching logic

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import './assets/Fonts/fonts.css'
 
