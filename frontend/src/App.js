086d2bacf3e894b38745b1278a30909e17e0408a
AZ0228 <53315675+AZ0228@users.noreply.github.com>
2024-04-24 12:01:44 -0400

    added analytics

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,28 +3,16 @@ import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
 import './App.css';
 import './assets/Fonts/fonts.css'
-import { gtagInit, pageview } from 'react-ga4';
+import ReactGA from "react-ga4";
 
 // pages
 import Home from './pages/Home/Home';
 import Commits from './pages/Commits/Commits';
 
-gtagInit({
-    measurementId: 'G-DEGDVYGCP0'
-});
-
-const useTrackPageViews = () => {
-    const location = useLocation();
-
-    useEffect(() => {
-        //tracks page view on path change
-        pageview(location.pathname + location.search);
-    }, [location]);
-};
-
+ReactGA.initialize("'G-DEGDVYGCP0'");
+ReactGA.send({ hitType: "pageview", page: window.location.pathname });
 
 function App() {
-    useTrackPageViews();
     return (
         <Router>
             <Routes>
