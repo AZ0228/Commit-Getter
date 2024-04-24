d67ddf6d5d44c4709fb543fa154d4234153df2e0
AZ0228 <53315675+AZ0228@users.noreply.github.com>
2024-04-24 11:50:31 -0400

    added google analytics:

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,37 @@
 import logo from './logo.svg';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import './App.css';
 import './assets/Fonts/fonts.css'
+import { gtagInit, pageview } from 'react-ga4';
 
 // pages
 import Home from './pages/Home/Home';
 import Commits from './pages/Commits/Commits';
 
+gtagInit({
+    measurementId: 'G-DEGDVYGCP0'
+});
+
+const useTrackPageViews = () => {
+    const location = useLocation();
+
+    useEffect(() => {
+        //tracks page view on path change
+        pageview(location.pathname + location.search);
+    }, [location]);
+};
+
+
 function App() {
+    useTrackPageViews();
     return (
-    <Router>
-        <Routes>
-            <Route path="/" exact element={<Home/>}/>
-            <Route path="/commits" element={<Commits/>}/>
-        </Routes>
-    </Router>
+        <Router>
+            <Routes>
+                <Route path="/" exact element={<Home />} />
+                <Route path="/commits" element={<Commits />} />
+            </Routes>
+        </Router>
     );
 }
 
